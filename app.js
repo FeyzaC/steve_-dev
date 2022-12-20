82ee0c37060f4e4bf96826cd07cc187b19e47527
gulfeyzaciftci <gulfeyza333@gmail.com>
2022-12-20 16:46:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// 1 ile 100 arasındaki sayıları 5 in katları halinde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 5 == 0) console.log(i);
+// }
+
+// 1 ile 100 arasındaki sayıları 5 in katları yazmayacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 5 !== 0) console.log(i);
+// }
+
+// 1 ile 100 arasındaki sayıları 5 ve 7 nin katları yazmayacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 5 !== 0 && i % 7 !== 0) console.log(i);
+// }
+
+// 1 ile 100 arasındaki sayıları 0 dan başlayıp 4 er artacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 4 == 0) console.log(i);
+// }
+
+// 1 ile 100 arasındaki sayıları 4 ve katları olmayacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 4 !== 0) console.log(i);
+// }
+
+// 1 ile 100 arasındaki ekrana yazılan sayıların, 20 ile 40 arasındaki sayıların 3 ün katları olacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i > 20 && i < 40 && i % 3 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 1 ile 100 arasındaki ekrana yazılan sayıların, 20 ile 40 arasındaki sayılar 3 ün katı olacak şekilde ekrana javascript kodları ile yazdırınız
+
+// for (let i = 0; i < 100; i++) {
+//   if (i > 20 && i < 40 && i % 3 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 1 ile 100 arasındaki ekrana yazılan sayıların, 20 ile 40 arasındaki sayıların 3 ün katları olmayanları ekrana javascript kodları ile yazdırınız
+
+for (let i = 0; i < 100; i++) {
+  if (i > 20 && i < 40 && i % 3 == 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+// 1 ile 100 arasındaki ekrana yazılan sayıların, 20 ile 40 arasındaki sayıların 3 ün katları olmayanları ekrana javascript kodları ile yazdırınız
